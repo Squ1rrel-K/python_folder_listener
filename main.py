babc238abaace643c119b9b52637d13c00b6fd6b
Squ1rrel-K <squ1rrel98k@gmail.com>
2022-07-21 22:33:38 +0800

    1.0.0

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-import sys
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from openpyxl import load_workbook
@@ -7,68 +6,80 @@ from openpyxl import load_workbook
 
 class SubFileHandler(FileSystemEventHandler):
     """
-    Those two variables will be replaced by user input
+    Those variables will be replaced by user input
     """
-    path_excel = 'F:/Code/test_folder_listener/test.xlsx'
+    excel_path = 'C:/Users/squ1rrel/Desktop/test/test.xlsx'
+    folder_path_tem = 'C:/Users/squ1rrel/Desktop/test/tarkov_tem'
+    folder_name_tem = folder_path_tem.split('\\')[-1]
+    folder_path = 'C:/Users/squ1rrel/Desktop/test/tarkov'
+    folder_name = folder_path.split('\\')[-1]
     sheet_name = 'Sheet1'
 
     def on_created(self, event):
-        print(event.event_type)
-        work_on_excel(self.path_excel, self.sheet_name, event)
-
-    '''
-    def on_modified(self, event):
-        print(event.event_type)
-        work_on_excel(self.path_excel, self.sheet_name, event.event_type)
-    '''
+        work_on_excel(self.excel_path, self.sheet_name, event, self.folder_name_tem, self.folder_name)
 
     def on_deleted(self, event):
-        print(event.event_type)
-        work_on_excel(self.path_excel, self.sheet_name, event)
+        work_on_excel(self.excel_path, self.sheet_name, event, None, self.folder_name)
 
 
-def work_on_excel(path_excel, sheet_name, event):
+def work_on_excel(excel_path, sheet_name, event, folder_tem_name, folder_name):
     try:
-        wb = load_workbook(path_excel)
+        wb = load_workbook(excel_path)
         try:
             ws = wb[sheet_name]
 
             if event.event_type == 'created':
-                video_created(ws, event)
+                work_on_excel_by_video_created(ws, event, folder_tem_name, folder_name)
 
             elif event.event_type == 'deleted':
-                video_deleted(ws)
+                work_on_excel_by_video_deleted(ws, event, folder_name)
 
-            wb.save(path_excel)
+            wb.save(excel_path)
 
         except IOError as msg:
             print(msg)
 
-    except IOError:
-        print('无法打开该 Excel 路径: ' + path_excel)
+    except IOError as msg:
+        print(msg)
 
 
-def video_created(work_sheet, event):
-    ws = work_sheet
-    current_row = ws.max_row + 1
-    relative_path = event.src_path.split('\\')[1]
-    ws.cell(column=1, row=current_row, value="{0}".format(relative_path)).hyperlink = event.src_path
-    ws.cell(column=4, row=current_row, value="No")
+def work_on_excel_by_video_created(work_sheet, event, folder_name_tem, folder_name):
+    current_row = work_sheet.max_row + 1
+    file_name = event.src_path.split('\\')[-1]
+    try:
+        work_sheet.cell(column=1, row=current_row, value="{0}".format(file_name)).hyperlink = event.src_path
+        work_sheet.cell(column=4, row=current_row, value="No")
+        print('Successful write: ' + '[' + file_name + ']' + ' From: ' + folder_name_tem + ' To ' + folder_name)
+
+    except IOError as msg:
+        print(msg)
 
 
-def video_modified(work_sheet):
-    print(1)
+def work_on_excel_by_video_deleted(work_sheet, event, folder_name):
+    try:
+        file_name = event.src_path.split('\\')[-1]
 
+        for row in range(2, work_sheet.max_row + 1):
+            if work_sheet.cell(column=1, row=row).internal_value is not None:
+                internal_value = work_sheet.cell(column=1, row=row).internal_value
 
-def video_deleted(work_sheet):
-    print(1)
+                if internal_value == file_name:
+                    work_sheet.delete_rows(idx=row)
+                    for row_sub in range(row, work_sheet.max_row + 1):
+                        work_sheet.cell(column=1, row=row_sub).hyperlink = \
+                            work_sheet.cell(column=1, row=row_sub).hyperlink.target
+
+        print('Successful delete: ' + '[' + file_name + ']' + ' From: ' + folder_name)
+
+    except IOError as msg:
+        print(msg)
 
 
 if __name__ == "__main__":
+    print('Welcome to Python_folder_listener!')
     observer = Observer()
     event_handler = SubFileHandler()
-    path_test_folder = "F:/Code/test_folder_listener"
-    observer.schedule(event_handler, path_test_folder, recursive=True)
+    observer.schedule(event_handler, event_handler.folder_path, recursive=True)
     observer.start()
     try:
         while True:
