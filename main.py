dd9c1789d4a6b005555538ee4b8f349b31725f31
Squ1rrel-K <squ1rrel98k@gmail.com>
2022-07-23 13:06:30 +0800

    optimise values

--- main.py
+++ main.py
@@ -11,31 +11,30 @@ class SubFileHandler(FileSystemEventHandler):
     excel_path = 'D:/素材/视频/塔科夫/12.13.xlsx'
     folder_path_tem = 'C:/Users/squ1rrel/Desktop/test/tarkov_tem'
     folder_path = 'D:/素材/视频/塔科夫/12.13'
-
     folder_name_tem = folder_path_tem.split('\\')[-1]
     folder_name = folder_path.split('\\')[-1]
     sheet_name = 'Sheet1'
 
     def on_created(self, event):
-        work_on_excel(self.excel_path, self.sheet_name, event, self.folder_name_tem, self.folder_name)
+        work_on_excel(self, event)
 
     def on_deleted(self, event):
-        work_on_excel(self.excel_path, self.sheet_name, event, None, self.folder_name)
+        work_on_excel(self, event)
 
 
-def work_on_excel(excel_path, sheet_name, event, folder_tem_name, folder_name):
+def work_on_excel(self, event):
     try:
-        wb = load_workbook(excel_path)
+        wb = load_workbook(self.excel_path)
         try:
-            ws = wb[sheet_name]
+            ws = wb[self.sheet_name]
 
             if event.event_type == 'created':
-                work_on_excel_by_event_created(ws, event, folder_tem_name, folder_name)
+                work_on_excel_by_event_created(self, ws, event)
 
             elif event.event_type == 'deleted':
-                work_on_excel_by_event_deleted(ws, event, folder_name)
+                work_on_excel_by_event_deleted(self, ws, event)
 
-            wb.save(excel_path)
+            wb.save(self.excel_path)
 
         except IOError as msg:
             print(msg)
@@ -44,33 +43,38 @@ def work_on_excel(excel_path, sheet_name, event, folder_tem_name, folder_name):
         print(msg)
 
 
-def work_on_excel_by_event_created(work_sheet, event, folder_name_tem, folder_name):
-    current_row = work_sheet.max_row + 1
+def work_on_excel_by_event_created(self, ws, event):
+    current_row = ws.max_row + 1
     file_name = event.src_path.split('\\')[-1]
     try:
-        work_sheet.cell(column=1, row=current_row, value="{0}".format(file_name)).hyperlink = event.src_path
-        work_sheet.cell(column=4, row=current_row, value="No")
-        print('Successful write: ' + '[' + file_name + ']' + ' From: ' + folder_name_tem + ' To ' + folder_name)
+        ws.cell(column=1, row=current_row, value="{0}".format(file_name)).hyperlink = event.src_path
+        ws.cell(column=4, row=current_row, value="No")
+        print('Successfully write: ' + '[' + file_name + ']'
+              + ' From: ' + self.folder_name_tem + ' To ' + self.folder_name)
 
     except IOError as msg:
         print(msg)
 
 
-def work_on_excel_by_event_deleted(work_sheet, event, folder_name):
+def work_on_excel_by_event_deleted(self, ws, event):
     try:
         file_name = event.src_path.split('\\')[-1]
 
-        for row in range(2, work_sheet.max_row + 1):
-            if work_sheet.cell(column=1, row=row).internal_value is not None:
-                internal_value = work_sheet.cell(column=1, row=row).internal_value
-
+        for row in range(2, ws.max_row + 1):
+            if ws.cell(column=1, row=row).internal_value is not None:
+                internal_value = ws.cell(column=1, row=row).internal_value
                 if internal_value == file_name:
-                    work_sheet.delete_rows(idx=row)
-                    for row_sub in range(row, work_sheet.max_row + 1):
-                        work_sheet.cell(column=1, row=row_sub).hyperlink = \
-                            work_sheet.cell(column=1, row=row_sub).hyperlink.target
+                    ws.delete_rows(idx=row)
 
-        print('Successful delete: ' + '[' + file_name + ']' + ' From: ' + folder_name)
+                    # This is a potential openpyxl bug,
+                    # that hyperlinks will stay at old places rather than going up after deleting rows,
+                    # but they still hold the right place at some point.
+                    # Have to concentrate their target again.
+                    for row_sub in range(row, ws.max_row + 1):
+                        ws.cell(column=1, row=row_sub).hyperlink = \
+                            ws.cell(column=1, row=row_sub).hyperlink.target
+
+        print('Successfully delete: ' + '[' + file_name + ']' + ' From: ' + self.folder_name)
 
     except IOError as msg:
         print(msg)
@@ -87,7 +91,7 @@ if __name__ == "__main__":
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print("关闭脚本")
+        print("Close!")
 
     finally:
         observer.stop()
