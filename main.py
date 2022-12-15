7b1458fab311b1ffd2d3ed65db426b8ae0da5234
Squ1rrel-K <squ1rrel98k@gmail.com>
2022-12-15 20:36:08 +0800

    add logging system and report last uploaded video name

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import time
 import json
+import logging
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from openpyxl import load_workbook
 
 
 class SubFileHandler(FileSystemEventHandler):
-    """
-    Those variables will be replaced by user input
-    """
     excel_path = None
     folder_path_tem = None
     folder_path = None
@@ -26,10 +24,11 @@ class SubFileHandler(FileSystemEventHandler):
         work_on_excel(self, event)
 
 
-def get_config(self, config_path="./config.json"):
+def get_config(self, config_path="./config_tarkov.json"):
     try:
-        f = open(config_path, mode='r', encoding='utf-8')
 
+        # Read from config.
+        f = open(config_path, mode='r', encoding='utf-8')
         config = json.loads(f.read())
         self.excel_path = config['excel_path']
         self.folder_path_tem = config['folder_path_tem']
@@ -37,11 +36,13 @@ def get_config(self, config_path="./config.json"):
         self.sheet_name = config['sheet_name']
         self.folder_name_tem = self.folder_path_tem.split('\\')[-1]
         self.folder_name = self.folder_path.split('\\')[-1]
-
         f.close()
 
+        # Print last uploaded file.
+        logging.info('Last file uploaded was' + ' [' + config['last_file'] + "]")
+
     except IOError as msg:
-        print(msg)
+        logging.error(msg)
 
 
 def work_on_excel(self, event):
@@ -59,24 +60,37 @@ def work_on_excel(self, event):
             wb.save(self.excel_path)
 
         except IOError as msg:
-            print(msg)
+            logging.error(msg)
 
     except IOError as msg:
-        print(msg)
+        logging.error(msg)
 
 
-def work_on_excel_by_event_created(self, ws, event):
+def work_on_excel_by_event_created(self, ws, event, config_path="./config_tarkov.json"):
     current_row = ws.max_row + 1
+    file_path = event.src_path.split('/')[-1]
     file_name = event.src_path.split('\\')[-1]
     try:
         # Modify these codes for private use.
-        ws.cell(column=1, row=current_row, value="{0}".format(file_name)).hyperlink = event.src_path
+        ws.cell(column=1, row=current_row, value="{0}".format(file_name)).hyperlink = file_path
         ws.cell(column=4, row=current_row, value="No")
-        print('Successfully write: ' + '[' + file_name + ']'
-              + ' From: ' + self.folder_name_tem + ' To ' + self.folder_name)
+
+        # Read from config.
+        f = open(config_path, mode='r', encoding='utf-8')
+        config = json.loads(f.read())
+        f.close()
+
+        # Rewrite config to update last file uploaded.
+        f = open(config_path, mode='w', encoding='utf-8')
+        config['last_file'] = file_name
+        f.write(json.dumps(config, indent=2, ensure_ascii=False))
+        f.close()
+
+        logging.info('Successfully write: ' + '[' + file_name + ']'
+                     + ' From: ' + self.folder_name_tem + ' To ' + self.folder_name)
 
     except IOError as msg:
-        print(msg)
+        logging.info(msg)
 
 
 def work_on_excel_by_event_deleted(self, ws, event):
@@ -97,21 +111,15 @@ def work_on_excel_by_event_deleted(self, ws, event):
                         ws.cell(column=1, row=row_sub).hyperlink = \
                             ws.cell(column=1, row=row_sub).hyperlink.target
 
-        print('Successfully delete: ' + '[' + file_name + ']' + ' From: ' + self.folder_name)
+        logging.info('Successfully delete: ' + '[' + file_name + ']' + ' From: ' + self.folder_name)
 
     except IOError as msg:
-        print(msg)
-
-
-def check_people_with_zero_material(self):
-    wb = load_workbook(self.excel_path)
-    ws = wb[self.sheet_name]
-
-    flag = True
+        logging.error(msg)
 
 
 if __name__ == "__main__":
     print('Welcome to Python_folder_listener!')
+    logging.basicConfig(level=logging.INFO)
     observer = Observer()
     event_handler = SubFileHandler()
     observer.schedule(event_handler, event_handler.folder_path, recursive=True)
@@ -121,7 +129,7 @@ if __name__ == "__main__":
             time.sleep(1)
 
     except KeyboardInterrupt:
-        print("Closed!")
+        logging.info("Closed!")
 
     finally:
         observer.stop()
